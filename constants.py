a67dda64284da474dc196df1175aee9dc6e5b6cc
Ben Pearson <blpearson44@icloud.com>
2021-10-09 15:33:21 -0700

    added constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+# Written by Ben Pearson
+# V0.0.1
+# This file is to store the constants for my program
+
+# Imports
+import numpy as np
+
+# Constants
+A = (1 * 10**(-15))
+V0 = 83
+C = (3 * 10**8)
+M = 940 / C
