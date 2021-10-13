636866291ebf4a97cdf384d12d280007c7bdea74
Ben Pearson <blpearson44@icloud.com>
2021-10-12 19:39:19 -0700

    Data into arrays

--- functions.py
+++ functions.py
@@ -1,24 +1,41 @@
 # Written by Ben Pearson
-# V0.0.1
+# V0.1.1
 # This file will hold the primary functions to be used in the program
 
 # Modules
 import numpy as np
+import constants as c
+
 
 # Newton's method
-# function
 def fx(x):
     return ( 3 * (1 - np.exp(-x)) - x )
-# derivative
+
 def dfx(x):
     return ( 3 * np.exp(-x) - 1 )
-# implement method
-def newton(func, dfunc, x):
+
+def newton_method(func, dfunc, x):
     return (x - func(x)/dfunc(x))
+
 
 # Functional Iteration
 def gx(x):
     return ( 3 * (1 - np.exp(-x)))
-# implement method
-def func_itor(func, x):
+def functional_iteration(func, x):
     return func(x)
+
+# Take function and iterate it c.N times 
+def newton_itor(x):
+    arr = [x]
+    for i in range(c.N):
+        x = newton_method(fx, dfx, x)
+        arr.append(x)
+    return arr
+
+# Take function and iterate it c.N times 
+def functional_itor(x):
+    arr = [x]
+    for i in range(c.N):
+        x = functional_iteration(gx, x)
+        arr.append(x)
+    return arr
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 # Written by Ben Pearson
-# V0.0.1
+# V0.0.2
 # This function will implement the main iterations and be responsible for delivering results
 
 # Modules
 import functions as f
 import constants as c
-x = 4
 
-print("Functional Iteration: x = " + str(x))
-for i in range(c.N):
-    x = f.func_itor(f.gx, x) 
-    print("x =\t" + str(x))
-    
 x = 2
-print("Newton's Method")
-for i in range(c.N):
-    x = f.newton(f.fx, f.dfx, x) 
-    print("x =\t" + str(x))
+print(f.functional_itor(x))
+print(f.newton_itor(x))
